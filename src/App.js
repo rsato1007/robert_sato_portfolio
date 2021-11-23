235da9baa6d5c4331dc2fe6d7ab000b57e99bdb4
rsato1007 <robertmsato@gmail.com>
2021-11-22 19:29:48 -0700

    testing deployment

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class LambdaDemo extends Component {
         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
         <br />
-        <span>{msg}</span>
+        <span>This is simply a test to see if this works.</span>
       </p>
     )
   }

2bccbe2e4f6ae8f7361a79805e37ef2df2da35a5
rsato1007 <robertmsato@gmail.com>
2021-11-24 19:35:49 -0700

    removing links to get build to deploy

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,10 +3,10 @@ import React from "react"
 const Navbar = () => {
     return (
         <div>
-            <a>Home</a>
+            {/* <a>Home</a>
             <a>About</a>
             <a>Projects</a>
-            <a>Contact</a>
+            <a>Contact</a> */}
         </div>
     )
 }

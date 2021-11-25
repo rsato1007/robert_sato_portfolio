5605d6284d84b8fc522a6e827dc08cdecd789533
rsato1007 <robertmsato@gmail.com>
2021-11-24 23:29:33 -0700

    adding style to about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,11 +9,11 @@ const About = () => {
         <div className="aboutContainer">
             <SectionHeader text="About"/>
             <TechLogos />
-            <div>
-                Robert Image
-            </div>
-            <div>
-                Born and raised in Colorado, I started my journey in finance, but after a few years, I was left unsatisfied. I enjoyed it, but I wanted something that would push me and allow to create things I can tangibly see. This is where coding comes in. I joined General Assembly’s software engineer program in May 2021 to start a new path and graudated in November 2021. A path where I can make things that empower others to achieve their purpose. Now I want bring my skills to company that empowers it’s users.
+            <div className="aboutMeInfoContainer">
+                <img className="robPic" src={require("../../images/robert.jpg")} alt="Picture of Robert" />
+                <p className="aboutMeText">
+                    Born and raised in Colorado, I started my journey in finance, but after a few years, I was left unsatisfied. I enjoyed it, but I wanted something that would push me and allow to create things I can tangibly see. This is where coding comes in. I joined General Assembly’s software engineer program in May 2021 to start a new path and graudated in November 2021. A path where I can make things that empower others to achieve their purpose. Now I want bring my skills to company that empowers it’s users.
+                </p>
             </div>
             <NextSectionButton text="Projects" />
         </div>

bf72fc7c7a0b1d2243f09b8d71fe5101c2739c6f
rsato1007 <robertmsato@gmail.com>
2021-11-27 16:04:04 -0700

    adding more projects and logos

--- src/components/About/TechLogos/index.js
+++ src/components/About/TechLogos/index.js
@@ -4,10 +4,10 @@ import "./style.css"
 const TechLogos = () => {
     return (
         <div className="logoContainer">
-            <img className="logo-image html-image" src={require("../../../images/HTML.png")} alt="HTML Logo" />
-            <img className="logo-image css-image" src={require("../../../images/CSS.png")} alt="CSS Logo" />
-            <img className="logo-image js-image" src={require("../../../images/js.png")} alt="JavaScript Logo" />
-            {/* <img className="logo-image" src={require("../../../images/react.png")} alt="React Logo" /> */}
+            <img className="logo-image html-image" src={require("../../../images/tech_logos/HTML.png")} alt="HTML Logo" />
+            <img className="logo-image css-image" src={require("../../../images/tech_logos/CSS.png")} alt="CSS Logo" />
+            <img className="logo-image js-image" src={require("../../../images/tech_logos/js.png")} alt="JavaScript Logo" />
+            <img className="logo-image react-image" src={require("../../../images/tech_logos/react.png")} alt="React Logo" />
         </div>
     )
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,9 +5,11 @@ import NextSectionButton from "../NextSectionButton";
 import "./style.css";
 
 const projectList = [
-    {title: "Community Bytes", tech: "HTML, CSS, EJS, Node.js, Express.js, MongoDB", description: "A recipe website that allows you to find, create, and review recipes.", Github: "https://github.com/rsato1007/Community-bytes", demo: "https://community-bytes.herokuapp.com/", image: require("../../images/project_image/Communitybytes.png")},
+    {title: "Community Bytes", tech: "HTML, CSS, EJS, Node.js, Express.js, MongoDB, Heroku, Google OAuth", description: "A recipe website that allows you to find, create, and review recipes.", Github: "https://github.com/rsato1007/Community-bytes", demo: "https://community-bytes.herokuapp.com/", image: require("../../images/project_image/Communitybytes.png")},
     {title: "Mario Party", tech: "HTML, CSS, JavaScript", description: "My very first project for GA! This was inspired by the iconic video game franchise.", Github: "https://github.com/rsato1007/rsato1007.github.io", demo: "https://rsato1007.github.io/", image: require("../../images/project_image/marioParty.png")},
-    {title: "The Commons", tech: "JWT Auth, HTML, CSS, MongoDB, React, Express.js, Node.js", description: "A group colloboration between me and three other people to build a social media app for people who don't like social media.", Github: "https://github.com/rsato1007/project-three", demo: "http://project3-app-flex525.herokuapp.com/", image: require("../../images/project_image/theCommons.png")}
+    {title: "The Commons", tech: "JWT Auth, HTML, CSS, MongoDB, React, Express.js, Node.js, Heroku", description: "A group colloboration between me and three other people to build a social media app for people who don't like social media.", Github: "https://github.com/rsato1007/project-three", demo: "http://project3-app-flex525.herokuapp.com/", image: require("../../images/project_image/theCommons.png")},
+    {title: "Historic Stops", tech: "Django, HTML, CSS, Postgresql, Heroku, AWS, JavaScript", description: "A collaboration between me and four other people to build a social media app for a client. An app with emphasis on local areas that historic.", Github: "https://github.com/rsato1007/wayfarer-project", demo: "https://historic-stops.herokuapp.com/", image: require("../../images/project_image/historicStops.png")},
+    {title: "Next Route", tech: "Django, HTML, CSS, Postgresql, Heroku, AWS, JavaScript", description: "A web app for rock climbers to find their next route, to share new routes, and to review current routes.", Github: "https://github.com/rsato1007/next_route", demo: "https://climbernextroute.herokuapp.com/", image: require("../../images/project_image/nextRoute.png")}
 ]
 
 const Projects = () => {
